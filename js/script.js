15bd69f14b60a696da06c7328d2a925f0d71ff2b
MizukiMori <mzk.72@email.plala.or.jp>
2021-05-14 23:50:56 +0900

    メニューバーの固定処理を完了。

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
+//フッターのトップに戻るボタン
 const arrow = document.getElementById("footer-arrow");
 arrow.addEventListener("click", () => window.scroll({top: 0, behavior: "smooth"}));
+
+
+//メニューバーをスクロールに合わせて動かす
+const menu = document.getElementById("menu");
+let menuTop = menu.offsetTop;
+window.addEventListener("scroll", fixMenu);
+
+function fixMenu(){
+	let currentScroll = scrollY;
+	if(menuTop < currentScroll){
+		menu.classList.add("fixed");
+	}else{
+		menu.classList.remove("fixed");
+	}
+}
