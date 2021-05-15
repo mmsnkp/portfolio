7f06a08e4b29f16ac30e5c096fbd58d762cc1ac0
MizukiMori <mzk.72@email.plala.or.jp>
2021-05-15 15:17:32 +0900

    作品の説明ページの作成を開始。

--- js/script.js
+++ js/script.js
@@ -3,6 +3,28 @@ const arrow = document.getElementById("footer-arrow");
 arrow.addEventListener("click", () => window.scroll({top: 0, behavior: "smooth"}));
 
 
+
+//メニュークリック時のスムーススクロール
+const menuAnchor = document.querySelectorAll(".menu-element a");
+menuAnchor.forEach((item) => item.addEventListener("click", smoothMenu));
+
+const documentURL = document.URL.split("#")[0];
+
+function smoothMenu(e){
+	let splittedURL = e.target.href.split("#");
+	if(2 > splittedURL.length){
+		return;
+	}
+	if(documentURL !== splittedURL[0]){
+		return;
+	}
+	e.preventDefault();
+	let target = document.querySelector("#" + splittedURL[1]);
+	window.scroll({top: target.offsetTop, behavior: "smooth"});
+}
+
+
+
 //メニューバーをスクロールに合わせて動かす
 const menu = document.getElementById("menu");
 let menuTop = menu.offsetTop;
